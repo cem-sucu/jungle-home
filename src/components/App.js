33d5d64b0ab2d8efa0302a61e3268d89f4806c93
cem-sucu <cems.cems.17@gmail.com>
2022-07-03 09:50:49 +0200

    add elements

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
-import React from "react";
-import Banner from "./Banner";
-import Cart from "./Cart";
-import ShoppingList from "./ShoppingList";
+import Banner from './Banner'
+import logo from '../assets/logo.png'
+// import Cart from './Cart'
+import Footer from './Footer'
+import ShoppingList from './ShoppingList'
 
-const App = () => {
-  return (
-    <div>
-      <Banner />
-      <Cart />
-      <ShoppingList />
-    </div>
-  );
-};
+function App() {
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
 
-export default App;
+			{/* <Cart /> */}
+			<ShoppingList />
+			<Footer />
+		</div>
+	)
+}
+
+export default App
--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -1,56 +1,84 @@
+import monstera from "../assets/monstera.jpg";
+import aloe from "../assets/aloe.jpg";
+import basilic from "../assets/basilic.jpg";
+import ficus from "../assets/ficus-lyrata.jpg";
+import geranium from "../assets/geranium.jpg";
+import olivier from "../assets/olivier.jpg";
+import pothos from "../assets/pothos-argenté.jpg";
+import succulente from "../assets/succulente.jpg";
+import yucca from "../assets/yucca.jpg";
+
 export const plantList = [
     {
         name: "monstera",
         category: "classique",
         id: "1ed",
-        isBestSale: true,
+        light: 2,
+        water: 3,
+        cover: monstera,
     },
     {
         name: "ficus lyrata",
         category: "classique",
         id: "2ab",
-        isBestSale: false,
+        light: 3,
+        water: 1,
+        cover: ficus,
     },
     {
         name: "pothos argenté",
         category: "classique",
         id: "3sd",
-        isBestSale: false,
+        light: 1,
+        water: 2,
+        cover: pothos,
     },
     {
         name: "yucca",
         category: "classique",
         id: "4kk",
-        isBestSale: false,
+        light: 3,
+        water: 1,
+        cover: yucca,
     },
     {
         name: "olivier",
         category: "extérieur",
         id: "5pl",
-        isBestSale: false,
+        light: 3,
+        water: 1,
+        cover: olivier,
     },
     {
         name: "géranium",
         category: "extérieur",
         id: "6uo",
-        isBestSale: false,
+        light: 2,
+        water: 2,
+        cover: geranium,
     },
     {
         name: "basilique",
         category: "extérieur",
         id: "7ie",
-        isBestSale: true,
+        light: 2,
+        water: 3,
+        cover: basilic,
     },
     {
         name: "aloe",
         category: "plante grasse",
         id: "8fp",
-        isBestSale: false,
+        light: 2,
+        water: 1,
+        cover: aloe,
     },
     {
         name: "succulente",
         category: "plante grasse",
         id: "9vn",
-        isBestSale: false, 
+        light: 2,
+        water: 1,
+        cover: succulente,
     },
 ];
